3f07f84187ea4cbcd9d0445b8a4031c31c7378df
Hector Duarte <hectorduarte240.hd@gmail.com>
2024-06-11 14:22:33 +0200

    Mejoras en boton Flotante & Se agrego boton de cambio de fondo

---
+++ js/modoOscuro.js
@@ -0,0 +1,8 @@
+let toggle=document.getElementById('toggle');
+toggle.addEventListener('change', (event)=>{
+    let checked=event.target.checked;
+    document.body.classList.toggle('dark');
+    if (checked==true){
+
+    }
+})
